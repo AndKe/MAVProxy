06d7da37b60f85822283b98432561cc694fd8a87
Andre Kjellstrup <andre.kjellstrup@gmail.com>
2018-02-17 15:33:33 +0100

    Speech warning for telemetry

--- MAVProxy/modules/mavproxy_console.py
+++ MAVProxy/modules/mavproxy_console.py
@@ -41,6 +41,7 @@ class ConsoleModule(mp_module.MPModule):
         self.unload_check_interval = 5 # seconds
         self.last_unload_check_time = time.time()
         self.add_command('console', self.cmd_console, "console module", ['add','list','remove'])
+        self.last_link_state = 0
         self.flaps_chan = 0 #values: 0=not detected yet, 5..14=flaps channel, 50=none found
         self.flaps_new = False # old parameter was RCx_ new is SERVOx
         mpstate.console = wxconsole.MessageConsole(title='Console')
@@ -299,11 +300,16 @@ class ConsoleModule(mp_module.MPModule):
             self.update_vehicle_menu()
 
         if type in ['RADIO', 'RADIO_STATUS']:
-            # handle RADIO msgs from all vehicles
             if msg.rssi < msg.noise+10 or msg.remrssi < msg.remnoise+10:
                 fg = 'red'
+                if self.last_link_state <> 1:
+                    self.say("telemetry link warning")
+                    self.last_link_state = 1
             else:
                 fg = 'black'
+                if self.last_link_state <> 0:
+                    self.say("telemetry link normal")
+                    self.last_link_state = 0
             self.console.set_status('Radio', 'Radio %u/%u %u/%u' % (msg.rssi, msg.noise, msg.remrssi, msg.remnoise), fg=fg)
 
         if type == 'SYS_STATUS':
